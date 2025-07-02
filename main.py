70cf326015dbdf3358a340e0c93ff6dee77c0f09
moroz <morozovdaniel713@gmail.com>
2025-07-02 20:59:41 +0300

    add docs and typing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import csv
+from typing import Any, Callable
 from abc import ABC, abstractmethod
 from tabulate import tabulate
 
@@ -9,15 +10,24 @@ from tabulate import tabulate
 class BaseReader(ABC):
 
     @abstractmethod
-    def read(self, file_name):
-        pass
+    def read(self, file_name: str) -> list[dict[str, str]]:
+        """Читает данные из файла и возвращает список словарей."""
 
 
 class BaseAggregate(ABC):
 
     @abstractmethod
-    def aggregate(self, data, aggregate_field):
-        pass
+    def aggregate(
+        self, data: list[dict[str, str | float]], aggregate_field: str
+    ) -> list[dict[str, str | float]]:
+        """Выполняет агрегацию данных по заданному полю."""
+
+    def check_type(self, field: dict[str, str | float], field_aggregate: str) -> None:
+        """Проверяет, можно ли преобразовать значение поля к числу."""
+        try:
+            float(field[field_aggregate])
+        except ValueError:
+            raise TypeError(f"Cannot sort by non-numeric field '{field_aggregate}'.")
 
 
 class BaseFilter(ABC):
@@ -25,38 +35,62 @@ class BaseFilter(ABC):
     IS_DIGIT = False
 
     @abstractmethod
-    def filter(self, data, filter_field, value):
-        pass
+    def filter(
+        self, data: list[dict[str, str]], filter_field: str, value: str
+    ) -> list[dict[str, str | float]]:
+        """Фильтрует данные по значению поля."""
 
-    def determine_type(self, value):
+    def determine_type(self, value: str) -> None:
+        """Применяет условие фильтрации к каждому элементу данных."""
         try:
             value = float(value)
             self.IS_DIGIT = True
         except (TypeError, ValueError):
             self.IS_DIGIT = False
 
+    def _apply_condition(
+        self,
+        data: list[dict[str, str]],
+        field: str,
+        value: str | int,
+        op: Callable[[str, str | float], bool],
+    ) -> list[dict[str, str]]:
+        result = []
+        for line in data:
+            if op(line[field], value):
+                result.append(line)
+        return result
+
 
 class BasePrint(ABC):
 
     @abstractmethod
-    def print(self, data):
-        pass
+    def print(self, data: list[dict[str, str | float]]) -> None:
+        """Выводит данные в консоль."""
 
 
 class BaseOrderBy(ABC):
 
+    def check_type(self, field: dict[str, str | float], field_order: str) -> None:
+        try:
+            float(field[field_order])
+        except ValueError:
+            raise TypeError(f"Cannot sort by non-numeric field '{field_order}'.")
+
     @abstractmethod
-    def order_by(self, data, field_order):
-        pass
+    def order_by(self, data, field_order) -> list[dict[str, str | float]]:
+        """Сортирует данные по указанному полю."""
 
 
 class CsvReader(BaseReader):
 
-    def read(self, file_name):
-        """reader with csv lib"""
+    def read(self, file_name: str) -> list[dict[str, str]]:
+        """Читает CSV-файл и возвращает список словарей."""
 
         with open(file=file_name, mode="r", encoding="utf-8") as csvfile:
             reader = list(csv.reader(csvfile, delimiter=","))
+            if not reader:
+                raise ValueError("The provided CSV file is empty.")
             fieldnames = reader[0]
             data = reader[1:]
 
@@ -70,105 +104,129 @@ class CsvReader(BaseReader):
 
 class FilterGT(BaseFilter):
 
-    def filter(self, data, filter_field, value):
-        result = []
+    def filter(
+        self, data: list[dict[str, str]], filter_field: str, value: str
+    ) -> list[dict[str, str]]:
+        """Фильтрует строки, где значение поля больше заданного."""
+
         super().determine_type(value)
         if self.IS_DIGIT:
-            for line in data:
-                if float(line[filter_field]) > float(value):
-                    result.append(line)
+            return self._apply_condition(
+                data, filter_field, float(value), lambda x, y: float(x) > y
+            )
         if not self.IS_DIGIT:
-            for line in data:
-                if line[filter_field] > value:
-                    result.append(line)
-        return result
+            return self._apply_condition(data, filter_field, value, lambda x, y: x > y)
 
 
 class FilterLT(BaseFilter):
 
-    def filter(self, data, filter_field, value):
-        result = []
+    def filter(
+        self, data: list[dict[str, str]], filter_field: str, value: str
+    ) -> list[dict[str, str]]:
+        """Фильтрует строки, где значение поля меньше заданного."""
+
         super().determine_type(value)
         if self.IS_DIGIT:
-            for line in data:
-                if float(line[filter_field]) < float(value):
-                    result.append(line)
+            return self._apply_condition(
+                data, filter_field, float(value), lambda x, y: float(x) < y
+            )
         if not self.IS_DIGIT:
-            for line in data:
-                if line[filter_field] < value:
-                    result.append(line)
-        return result
+            return self._apply_condition(data, filter_field, value, lambda x, y: x < y)
 
 
 class FilterEq(BaseFilter):
 
-    def filter(self, data, filter_field, value):
-        result = []
+    def filter(
+        self, data: list[dict[str, str]], filter_field: str, value: str
+    ) -> list[dict[str, str]]:
+        """Фильтрует строки, где значение поля равно заданному."""
+
         super().determine_type(value)
         if self.IS_DIGIT:
-            for line in data:
-                if float(line[filter_field]) == float(value):
-                    result.append(line)
+            return self._apply_condition(
+                data, filter_field, float(value), lambda x, y: float(x) == y
+            )
         if not self.IS_DIGIT:
-            for line in data:
-                if line[filter_field] == value:
-                    result.append(line)
-        return result
+            return self._apply_condition(data, filter_field, value, lambda x, y: x == y)
 
 
 class AggregateAvg(BaseAggregate):
 
-    def aggregate(self, data, aggregate_field):
+    def aggregate(
+        self, data: list[dict[str, str | float]], aggregate_field: str
+    ) -> list[dict[str, float | int]]:
+        """Вычисляет среднее значение по указанному полю."""
 
         result = []
         count = len(data)
 
-        for line in data:
-            result.append(float(line[aggregate_field]))
+        for field in data:
+            super().check_type(field, aggregate_field)
+            result.append(float(field[aggregate_field]))
         return [{"avg": round(sum(result) / count, 2)}]
 
 
 class AggregateMin(BaseAggregate):
 
-    def aggregate(self, data, aggregate_field):
+    def aggregate(
+        self, data: list[dict[str, str | float]], aggregate_field: str
+    ) -> list[dict[str, float | int]]:
+        """Вычисляет минимальное значение по указанному полю."""
 
         result = []
         count = len(data)
 
-        for line in data:
-            result.append(float(line[aggregate_field]))
+        for field in data:
+            super().check_type(field, aggregate_field)
+            result.append(float(field[aggregate_field]))
         return [{"min": min(result)}]
 
 
 class AggregateMax(BaseAggregate):
 
-    def aggregate(self, data, aggregate_field):
+    def aggregate(
+        self, data: list[dict[str, str | float]], aggregate_field: str
+    ) -> list[dict[str, float | int]]:
+        """Вычисляет максимальное значение по указанному полю."""
 
         result = []
         count = len(data)
 
-        for line in data:
-            result.append(float(line[aggregate_field]))
+        for field in data:
+            super().check_type(field, aggregate_field)
+            result.append(float(field[aggregate_field]))
         return [{"max": max(result)}]
 
 
 class OrderByAsc(BaseOrderBy):
 
-    def order_by(self, data, field_order):
+    def order_by(
+        self, data: list[dict[str, str | float]], field_order: str
+    ) -> list[dict[str, str | float]]:
+        """Сортирует данные по возрастанию по указанному полю."""
 
+        for field in data:
+            super().check_type(field, field_order)
         return sorted(data, key=lambda field: float(field[field_order]))
 
 
 class OrderByDesc(BaseOrderBy):
 
-    def order_by(self, data, field_order):
+    def order_by(
+        self, data: list[dict[str, str | float]], field_order: str
+    ) -> list[dict[str, str | float]]:
+        """Сортирует данные по убыванию по указанному полю."""
 
+        for field in data:
+            super().check_type(field, field_order)
         return sorted(data, key=lambda field: float(field[field_order]), reverse=True)
 
 
 class TablePrettyPrint(BasePrint):
 
-    def print(self, data):
+    def print(self, data: list[dict[str, str | float | int]]) -> None:
+        """Выводит данные в виде таблицы."""
+
         table = [field.values() for field in data]
         headers = [i for i in data[0]]
         print(tabulate(table, headers, tablefmt="pretty"))
@@ -189,40 +247,39 @@ if __name__ == "__main__":
     aggregate_field = args.aggregate
     order_by_field = args.order_by
 
-    allowed_filter_parameters = {
-        ">": FilterGT,
-        "<": FilterLT,
-        "=": FilterEq
-    }
+    allowed_filter_parameters = {">": FilterGT, "<": FilterLT, "=": FilterEq}
     allowed_aggregate_values = {
         "avg": AggregateAvg,
         "min": AggregateMin,
         "max": AggregateMax,
     }
-    allowed_order_by_parameters = {
-        "asc": OrderByAsc,
-        "desc": OrderByDesc
-    }
+    allowed_order_by_parameters = {"asc": OrderByAsc, "desc": OrderByDesc}
 
     data = CsvReader().read(file_name)
 
     if filter_field is not None:
         for parameter in allowed_filter_parameters:
             if parameter in filter_field:
-                filter_paremeter = parameter
+                filter_parameter = parameter
                 break
         else:
-            pass
-            # RAISE
-        field, value = filter_field.split(filter_paremeter)
-        data = allowed_filter_parameters[filter_paremeter]().filter(data, field, value)
+            raise ValueError(
+                f"Unsupported filter condition in '{filter_field}'. Allowed: {list(allowed_filter_parameters)}"
+            )
+
+        field, value = filter_field.split(filter_parameter)
+        data = allowed_filter_parameters[filter_parameter]().filter(data, field, value)
 
     if aggregate_field is not None:
         field, type_aggregate = aggregate_field.split("=")
         data = allowed_aggregate_values[type_aggregate]().aggregate(data, field)
+    if type_aggregate not in allowed_aggregate_values:
+        raise ValueError(
+            f"Unsupported aggregate type '{type_aggregate}'. Allowed: {list(allowed_aggregate_values)}"
+        )
 
     if order_by_field is not None:
-        field, order_by_paremeter = order_by_field.split("=")
-        data = allowed_order_by_parameters[order_by_paremeter]().order_by(data, field)
+        field, order_by_parameter = order_by_field.split("=")
+        data = allowed_order_by_parameters[order_by_parameter]().order_by(data, field)
 
     TablePrettyPrint().print(data)
