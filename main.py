0a6c63d5b0afb82264357f92e64c4728b73721f4
moroz <morozovdaniel713@gmail.com>
2025-07-02 19:06:45 +0300

    finish: finish functional for quick add cli-command, filters, ordering, aggregate

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from tabulate import tabulate
 
 
 class BaseReader(ABC):
-    
+
     @abstractmethod
     def read(self, file_name):
         pass
 
 
-class Aggregate(ABC):
+class BaseAggregate(ABC):
 
     @abstractmethod
     def aggregate(self, data, aggregate_field):
@@ -36,10 +36,17 @@ class BaseFilter(ABC):
             self.IS_DIGIT = False
 
 
-class Print(ABC):
+class BasePrint(ABC):
 
     @abstractmethod
     def print(self, data):
+        pass
+
+
+class BaseOrderBy(ABC):
+
+    @abstractmethod
+    def order_by(self, data, field_order):
         pass
 
 
@@ -65,10 +72,10 @@ class FilterGT(BaseFilter):
 
     def filter(self, data, filter_field, value):
         result = []
-        self.determine_type(value)
+        super().determine_type(value)
         if self.IS_DIGIT:
             for line in data:
-                if float(line[filter_field]) > value:
+                if float(line[filter_field]) > float(value):
                     result.append(line)
         if not self.IS_DIGIT:
             for line in data:
@@ -81,10 +88,10 @@ class FilterLT(BaseFilter):
 
     def filter(self, data, filter_field, value):
         result = []
-        self.determine_type(value)
+        super().determine_type(value)
         if self.IS_DIGIT:
             for line in data:
-                if float(line[filter_field]) < value:
+                if float(line[filter_field]) < float(value):
                     result.append(line)
         if not self.IS_DIGIT:
             for line in data:
@@ -97,10 +104,10 @@ class FilterEq(BaseFilter):
 
     def filter(self, data, filter_field, value):
         result = []
-        self.determine_type(value)
+        super().determine_type(value)
         if self.IS_DIGIT:
             for line in data:
-                if float(line[filter_field]) == value:
+                if float(line[filter_field]) == float(value):
                     result.append(line)
         if not self.IS_DIGIT:
             for line in data:
@@ -109,48 +116,59 @@ class FilterEq(BaseFilter):
         return result
 
 
-class AggregateAvg(Aggregate):
+class AggregateAvg(BaseAggregate):
 
     def aggregate(self, data, aggregate_field):
-        aggr_field = aggregate_field.split("=")[0]
 
         result = []
         count = len(data)
 
         for line in data:
-            result.append(float(line[aggr_field]))
+            result.append(float(line[aggregate_field]))
         return [{"avg": round(sum(result) / count, 2)}]
 
 
-class AggregateMin(Aggregate):
+class AggregateMin(BaseAggregate):
 
     def aggregate(self, data, aggregate_field):
-        aggr_field = aggregate_field.split("=")[0]
 
         result = []
         count = len(data)
 
         for line in data:
-            result.append(float(line[aggr_field]))
+            result.append(float(line[aggregate_field]))
         return [{"min": min(result)}]
 
 
-class AggregateMax(Aggregate):
+class AggregateMax(BaseAggregate):
 
     def aggregate(self, data, aggregate_field):
-        aggr_field = aggregate_field.split("=")[0]
 
         result = []
         count = len(data)
 
         for line in data:
-            result.append(float(line[aggr_field]))
+            result.append(float(line[aggregate_field]))
         return [{"max": max(result)}]
 
 
-class TablePrettyPrint(Print):
+class OrderByAsc(BaseOrderBy):
 
-    def pretty_print(self, data):
+    def order_by(self, data, field_order):
+
+        return sorted(data, key=lambda field: float(field[field_order]))
+
+
+class OrderByDesc(BaseOrderBy):
+
+    def order_by(self, data, field_order):
+
+        return sorted(data, key=lambda field: float(field[field_order]), reverse=True)
+
+
+class TablePrettyPrint(BasePrint):
+
+    def print(self, data):
         table = [field.values() for field in data]
         headers = [i for i in data[0]]
         print(tabulate(table, headers, tablefmt="pretty"))
@@ -162,16 +180,49 @@ if __name__ == "__main__":
     parser.add_argument("--file", type=str, required=True, help="file")
     parser.add_argument("--where", default=None, type=str, help="filter")
     parser.add_argument("--aggregate", default=None, type=str, help="aggregate")
+    parser.add_argument("--order_by", default=None, type=str, help="order_by")
 
     args = parser.parse_args()
 
     file_name = args.file
     filter_field = args.where
     aggregate_field = args.aggregate
+    order_by_field = args.order_by
 
-    # data = read(file_name=file_name)
-    # if filter_field is not None:
-    #     data = filter(data=data, filter_field=filter_field)
-    # if aggregate_field is not None:
-    #     data = aggregate(data=data, aggregate_field=aggregate_field)
-    # pretty_print(data)
+    allowed_filter_parameters = {
+        ">": FilterGT,
+        "<": FilterLT,
+        "=": FilterEq
+    }
+    allowed_aggregate_values = {
+        "avg": AggregateAvg,
+        "min": AggregateMin,
+        "max": AggregateMax,
+    }
+    allowed_order_by_parameters = {
+        "asc": OrderByAsc,
+        "desc": OrderByDesc
+    }
+
+    data = CsvReader().read(file_name)
+
+    if filter_field is not None:
+        for parameter in allowed_filter_parameters:
+            if parameter in filter_field:
+                filter_paremeter = parameter
+                break
+        else:
+            pass
+            # RAISE
+        field, value = filter_field.split(filter_paremeter)
+        data = allowed_filter_parameters[filter_paremeter]().filter(data, field, value)
+
+    if aggregate_field is not None:
+        field, type_aggregate = aggregate_field.split("=")
+        data = allowed_aggregate_values[type_aggregate]().aggregate(data, field)
+
+    if order_by_field is not None:
+        field, order_by_paremeter = order_by_field.split("=")
+        data = allowed_order_by_parameters[order_by_paremeter]().order_by(data, field)
+
+    TablePrettyPrint().print(data)
