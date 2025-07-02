202b73a83fddec8cad6304628b2b287f322aec96
moroz <morozovdaniel713@gmail.com>
2025-07-02 17:45:14 +0300

    laid the possibility of quickly adding new types of aggregation, filtering, reading various files, and also various outputs of information

--- main.py
+++ main.py
@@ -2,90 +2,158 @@
 
 import argparse
 import csv
+from abc import ABC, abstractmethod
 from tabulate import tabulate
 
 
-def read_csv(file_name):
-    """reader with csv lib"""
-
-    with open(file=file_name, mode="r", encoding="utf-8") as csvfile:
-        reader = list(csv.reader(csvfile, delimiter=","))
-        fieldnames = reader[0]
-        data = reader[1:]
-
-        data = [
-            {fieldnames[j]: data[i][j] for j in range(len(fieldnames))}
-            for i in range(len(data))
-        ]
-
-        return data
+class BaseReader(ABC):
+    
+    @abstractmethod
+    def read(self, file_name):
+        pass
 
 
-def filter(data, filter_field):
-    allowed_operators = ["<", ">", "="]
-    is_digit = False
+class Aggregate(ABC):
 
-    index_operator = [
-        filter_field.find(oper) for oper in allowed_operators if oper in filter_field
-    ][0]
-    operator = filter_field[index_operator]
-    field, value = filter_field.split(operator)
+    @abstractmethod
+    def aggregate(self, data, aggregate_field):
+        pass
 
-    try:
-        value = float(value)
-        is_digit = True
-    except (TypeError, ValueError) as e:
-        value = str(value)
 
-    result = []
-    for line in data:
-        if is_digit:
-            if operator == "<":
-                if float(line[field]) < value:
+class BaseFilter(ABC):
+
+    IS_DIGIT = False
+
+    @abstractmethod
+    def filter(self, data, filter_field, value):
+        pass
+
+    def determine_type(self, value):
+        try:
+            value = float(value)
+            self.IS_DIGIT = True
+        except (TypeError, ValueError):
+            self.IS_DIGIT = False
+
+
+class Print(ABC):
+
+    @abstractmethod
+    def print(self, data):
+        pass
+
+
+class CsvReader(BaseReader):
+
+    def read(self, file_name):
+        """reader with csv lib"""
+
+        with open(file=file_name, mode="r", encoding="utf-8") as csvfile:
+            reader = list(csv.reader(csvfile, delimiter=","))
+            fieldnames = reader[0]
+            data = reader[1:]
+
+            data = [
+                {fieldnames[j]: data[i][j] for j in range(len(fieldnames))}
+                for i in range(len(data))
+            ]
+
+            return data
+
+
+class FilterGT(BaseFilter):
+
+    def filter(self, data, filter_field, value):
+        result = []
+        self.determine_type(value)
+        if self.IS_DIGIT:
+            for line in data:
+                if float(line[filter_field]) > value:
                     result.append(line)
-            if operator == ">":
-                if float(line[field]) > value:
+        if not self.IS_DIGIT:
+            for line in data:
+                if line[filter_field] > value:
                     result.append(line)
-            if operator == "=":
-                if float(line[field]) == value:
-                    result.append(line)
-        else:
-            if operator == "<":
-                if line[field] < value:
-                    result.append(line)
-            if operator == ">":
-                if line[field] > value:
-                    result.append(line)
-            if operator == "=":
-                if line[field] == value:
-                    result.append(line)
-    return result
+        return result
 
 
-def aggregate(data, aggregate_field):
-    aggr_field, type_aggr = aggregate_field.split("=")
+class FilterLT(BaseFilter):
 
-    result = []
-    count = len(data)
-    if type_aggr == "avg":
+    def filter(self, data, filter_field, value):
+        result = []
+        self.determine_type(value)
+        if self.IS_DIGIT:
+            for line in data:
+                if float(line[filter_field]) < value:
+                    result.append(line)
+        if not self.IS_DIGIT:
+            for line in data:
+                if line[filter_field] < value:
+                    result.append(line)
+        return result
+
+
+class FilterEq(BaseFilter):
+
+    def filter(self, data, filter_field, value):
+        result = []
+        self.determine_type(value)
+        if self.IS_DIGIT:
+            for line in data:
+                if float(line[filter_field]) == value:
+                    result.append(line)
+        if not self.IS_DIGIT:
+            for line in data:
+                if line[filter_field] == value:
+                    result.append(line)
+        return result
+
+
+class AggregateAvg(Aggregate):
+
+    def aggregate(self, data, aggregate_field):
+        aggr_field = aggregate_field.split("=")[0]
+
+        result = []
+        count = len(data)
+
         for line in data:
             result.append(float(line[aggr_field]))
         return [{"avg": round(sum(result) / count, 2)}]
-    if type_aggr == "min":
+
+
+class AggregateMin(Aggregate):
+
+    def aggregate(self, data, aggregate_field):
+        aggr_field = aggregate_field.split("=")[0]
+
+        result = []
+        count = len(data)
+
         for line in data:
             result.append(float(line[aggr_field]))
         return [{"min": min(result)}]
-    if type_aggr == "max":
+
+
+class AggregateMax(Aggregate):
+
+    def aggregate(self, data, aggregate_field):
+        aggr_field = aggregate_field.split("=")[0]
+
+        result = []
+        count = len(data)
+
         for line in data:
             result.append(float(line[aggr_field]))
         return [{"max": max(result)}]
 
 
-def pretty_print(data):
-    table = [field.values() for field in data]
-    headers = [i for i in data[0]]
-    print(tabulate(table, headers, tablefmt="pretty"))
-    
+class TablePrettyPrint(Print):
+
+    def pretty_print(self, data):
+        table = [field.values() for field in data]
+        headers = [i for i in data[0]]
+        print(tabulate(table, headers, tablefmt="pretty"))
 
 
 if __name__ == "__main__":
@@ -101,9 +169,9 @@ if __name__ == "__main__":
     filter_field = args.where
     aggregate_field = args.aggregate
 
-    data = read_csv(file_name=file_name)
-    if filter_field is not None:
-        data = filter(data=data, filter_field=filter_field)
-    if aggregate_field is not None:
-        data = aggregate(data=data, aggregate_field=aggregate_field)
-    pretty_print(data)
+    # data = read(file_name=file_name)
+    # if filter_field is not None:
+    #     data = filter(data=data, filter_field=filter_field)
+    # if aggregate_field is not None:
+    #     data = aggregate(data=data, aggregate_field=aggregate_field)
+    # pretty_print(data)
